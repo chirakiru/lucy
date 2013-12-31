d03783f84412f0ccda08ae8f8828451ed209372c
Uriel Molina <uriel85@gmail.com>
2013-12-30 18:49:07 -0600

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unrespiroamiciudad::Application.config.secret_key_base = '0355e96773cc199f1a5443f6f887d97d9f1506f1cd73db46b4f94fe70c1917a496a150dc15552801f0ef0d47214a2fefbe4fa663fcd18c2a2b78126d0c5887c3'
