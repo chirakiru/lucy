16fbbf486387b426433a6d94d7e85a005a36b092
urielable <uriel85@gmail.com>
2016-04-29 11:36:12 -0500

    Adds quotes section.

--- coffee/lucy.coffee
+++ coffee/lucy.coffee
@@ -1,7 +1,24 @@
-$(document).ready ->
+AddQuote = (->  
+  _initialize = () ->
+    do _add_to_list
+
+  _add_to_list = () ->
+    _quote_div  = $('#quote')
+    $.getJSON '../air_quotes.json', (data) ->
+      entry = data[Math.floor(Math.random() * data.length)]
+      content = "<h5>#{entry.quote}</h5><h4>- #{entry.author}</h4>"
+      _quote_div.append content
+      return
+
+  add: _initialize
+)()
+
+$(document).on 'page:load ready', ->
   map = L.map('mtymap',
     center: [
       51.505
       -0.09
     ]
     zoom: 13)
+  console.log 'jjjj'
+  AddQuote.add()
--- js/lucy.js
+++ js/lucy.js
@@ -1,7 +1,31 @@
-$(document).ready(function() {
+var AddQuote;
+
+AddQuote = (function() {
+  var _add_to_list, _initialize;
+  _initialize = function() {
+    return _add_to_list();
+  };
+  _add_to_list = function() {
+    var _quote_div;
+    _quote_div = $('#quote');
+    return $.getJSON('../air_quotes.json', function(data) {
+      var content, entry;
+      entry = data[Math.floor(Math.random() * data.length)];
+      content = "<h5>" + entry.quote + "</h5><h4>- " + entry.author + "</h4>";
+      _quote_div.append(content);
+    });
+  };
+  return {
+    add: _initialize
+  };
+})();
+
+$(document).on('page:load ready', function() {
   var map;
-  return map = L.map('mtymap', {
+  map = L.map('mtymap', {
     center: [51.505, -0.09],
     zoom: 13
   });
+  console.log('jjjj');
+  return AddQuote.add();
 });
