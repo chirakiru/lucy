4e435dfe9c070bcf7d8781aac8fff5d7630968a6
urielable <uriel85@gmail.com>
2016-05-28 01:43:38 -0500

    Change disqus script for change channel to page

--- src/js/disqus.js
+++ src/js/disqus.js
@@ -8,7 +8,7 @@ var disqus_config = function () {
 (function() {  // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
 
-    s.src = '//channel-unrespiroamiciudad.disqus.com/embed.js';
+    s.src = '//unrespiroamiciudad.disqus.com/embed.js';
 
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
